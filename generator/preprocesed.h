9c6021d025e2e4d0e4500a9c02284ce35af1ab9e
Wolfleader <james.morgan@iinet.net.au>
2022-03-28 11:49:18 +0800

    working version of cimgui

---
+++ generator/preprocesed.h
@@ -0,0 +1,9 @@
+
+ bool ImGui_ImplOpenGL2_Init();
+ void ImGui_ImplOpenGL2_Shutdown();
+ void ImGui_ImplOpenGL2_NewFrame();
+ void ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
+ bool ImGui_ImplOpenGL2_CreateFontsTexture();
+ void ImGui_ImplOpenGL2_DestroyFontsTexture();
+ bool ImGui_ImplOpenGL2_CreateDeviceObjects();
+ void ImGui_ImplOpenGL2_DestroyDeviceObjects();
